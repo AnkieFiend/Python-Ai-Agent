b61fa66b6ede62c87c564affd6499cf4c7745a0c
AnkieFiend <schema-velvet-0t@icloud.com>
2025-08-08 11:59:50 -0700

    re-organize code to make it more readable. Store user prompts. Add --verbose command line argument to add prompt and token details.

--- main.py
+++ main.py
@@ -2,27 +2,51 @@ import os
 from dotenv import load_dotenv
 import sys
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents= sys.argv)
-
-
-
+from google.genai import types
 
 def main():
-    print("Hello from ai-agent-build!")
-    if len(sys.argv) < 2:
-        print("error: please provide a prompt",  file = sys.stderr)
-        sys.exit(1)
-    elif len(sys.argv) >= 2:
-        print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    load_dotenv()
 
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    if not args:
+        print("Error: Please provide a prompt",  file = sys.stderr)
+        sys.exit(1)
+    
+    if verbose:
+        print(f"User prompt: {user_prompt} \n")
+    
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents= messages
+    )
+    print("Response:")
+    print(response.text)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 
 
 if __name__ == "__main__":
     main()
+
+
+
